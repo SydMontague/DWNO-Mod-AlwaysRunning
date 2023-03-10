cf591e5fae51247814ad806aa669ef8d4ad96fe5
SydMontague <sydmontague@phoenix-staffel.de>
2023-03-10 23:25:35 +0100

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace AlwaysRunning
+{
+    class Patches
+    {
+        [HarmonyPatch(typeof(PlayerCtrl), "Walking")]
+        [HarmonyPrefix]
+        static bool Walking_Prefix(PlayerCtrl __instance)
+        {
+            if (__instance.m_reqAction != 0)
+            {
+                __instance.RequestAction();
+            }
+            else
+            {
+                float speed = Plugin.Instance.runningSpeed.Value;
+
+                if (PadManager.IsInput(PadManager.BUTTON.bCross))
+                    speed = Plugin.Instance.walkingSpeed.Value;
+
+                __instance.m_move.SetAccelerationRate(speed);
+                __instance.m_isMoved = __instance.m_move.SetPlayerMove();
+                __instance.LookAtSomethingOnField();
+            }
+            return false;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using System.ComponentModel;
+
+namespace AlwaysRunning;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("Digimon World Next Order.exe")]
+public class Plugin : BasePlugin
+{
+    public static Plugin Instance { get; private set; }
+
+    public ConfigEntry<float> runningSpeed { get; private set;  }
+    public ConfigEntry<float> walkingSpeed { get; private set; }
+
+    public override void Load()
+    {
+        Instance = this;
+
+        runningSpeed = Config.Bind("General", "RunningSpeed", 1.4f, "Speed modifier while running.");
+        walkingSpeed = Config.Bind("General", "WalkingSpeed", 1.0f, "Speed modifier while walking.");
+
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        Harmony.CreateAndPatchAll(typeof(Patches));
+    }
+}
